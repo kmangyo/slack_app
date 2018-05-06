c1388b78c2cef2404543dc9b62ab64f00b486e7c
Hyun Gyo, Kim <khg423@gmail.com>
2018-05-06 18:27:55 +0900

    Update daily_slack_twit.r

--- slack_twit/daily_slack_twit.r
+++ slack_twit/daily_slack_twit.r
@@ -1,20 +1,28 @@
+# https://api.slack.com/custom-integrations/legacy-tokens
 # devtools::install_github("bnosac/cronR")
-library(cronR)
 # install.packages('miniUI')
 # install.packages('shiny')
 # install.packages('shinyFiles')
+# install.packages('slackr')
+# install.packages('googleLanguageR')
+# install.packages('stringr')
+# install.packages("reshape2")
 
+library(googleLanguageR)
+library(stringr)
+library(cronR)
 library(slackr)
 library(dplyr)
+library(reshape2)
+library(ggplot2)
 
-twit<-read.csv("twit_data.csv")
-twit$date<-as.Date(twit$date)
+twit<-read.csv("tweets_kmangyo.csv")
+twit$date<-as.Date(twit$timestamp)
 
-slackrSetup(config_file='slack.slackr')
-today <- as.Date(Sys.time(),tz='KST')
+today <- as.Date(Sys.time(),tz='Asia/Seoul')
 
 day <- list()
-for(i in 1:7){
+for(i in 1:8){
   day[i] <-as.Date(today - (365*i))
 }
 
@@ -23,9 +31,56 @@ for (i in 1:length(day)){
   memory[[i]]<- subset(twit, date==as.Date(day[[i]], origin='1970-01-01'))
 }
 
+slackr_setup(config_file='slack.slackr')
+
 for(i in 1:length(memory)){
-  slackr(print(memory[[i]]$date[1]))
+  slackr(as.character(memory[[i]]$date[1]),channel = '#memory')
   Sys.sleep(5)
-  slackr(as.character(memory[[i]]$text))
+  slackr(as.character(memory[[i]]$text,channel = '#memory'))
   Sys.sleep(5)
 }
+
+myword <- list()
+
+for(i in 1:length(memory)){
+  myword[[i]] <- subset(memory[[i]], str_detect(text, '^RT @')==FALSE)
+}
+
+gl_auth("XXXX.json")
+
+snt_result <- list()
+for(i in 1:length(memory)){
+  snt_result[[i]] <- gl_nlp(as.character(myword[[i]]$text), language = "ko")
+}
+
+vplot <- list()
+for(i in 1:length(memory)){
+ vplot[[i]] <- snt_result[[i]]$documentSentiment$score
+}
+
+y_date <- list()
+for(i in 1:length(memory)){
+  y_date[[i]] <- myword[[i]]$date
+}
+
+# Null issues (only rt day) 
+for(i in 1:length(memory)) {
+  if (is.null(vplot[[i]]) == TRUE) {
+    vplot[[i]] <- NA 
+  } else {
+    vplot[[i]] <- vplot[[i]]
+  }
+}
+
+vplot_df <- melt(vplot)
+vplot_df <- subset(vplot_df, value!=c('NA'))
+y_date <- melt(y_date)
+  
+vplot_df <- cbind(vplot_df, y_date)
+names(vplot_df)[3] <- 'date'
+
+# cnt twit
+ggslackr(ggplot(vplot_df, aes(x=date)) +geom_bar(stat="count"))
+
+# boxplot smt
+ggslackr(ggplot(vplot_df, aes(x=as.factor(substr(vplot_df$date,1,4)), y=value)) + geom_boxplot())
